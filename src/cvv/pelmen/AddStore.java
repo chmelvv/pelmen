42b4d63935944295cbff5efeac56e22d74e2714f
chmelvv <viktor.chmel@gmail.com>
2013-08-15 19:09:41 +0300

    [add] AddStore activity <=> DB frontend

---
+++ src/cvv/pelmen/AddStore.java
@@ -0,0 +1,19 @@
+package cvv.pelmen;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddStore extends Activity  {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_store);
+}
+}
+
